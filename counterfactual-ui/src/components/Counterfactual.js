e8dc0dd37875b5b7bd3b7890eda8acb2bab79722
CLendering <camile.lendering@gmail.com>
2024-05-23 12:54:24 +0200

    UI functional

--- counterfactual-ui/src/components/Counterfactual.js
+++ counterfactual-ui/src/components/Counterfactual.js
@@ -8,7 +8,6 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
   const [features, setFeatures] = useState(inputFeatures);
 
   useEffect(() => {
-    console.log("Counterfactual updated:", counterfactual);
     setSelectedCounterfactual({ ...counterfactual, hiddenFeatures: [], features: inputFeatures });
   }, [counterfactual, inputFeatures]);
 
@@ -22,9 +21,6 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
   };
 
   useEffect(() => {
-    console.log("Dataset name:", datasetName);
-    console.log("Model name:", modelName);
-    console.log("Target variable:", targetVariable);
 
     // fetch the dataset based on the dataset name
     const fetchData = async () => {
@@ -32,18 +28,15 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
         const response = await fetch('http://localhost:8000/datasets/');
         const data = await response.json();
         //find the dataset with the name datasetName
-        console.log(Object.values(data.datasets));
         const dataset = Object.values(data.datasets).find(d => d.title === "processed_data"); //TODO: change to datasetName
 
         if (!dataset) {
           console.error("Dataset not found");
           return;
         }
-        console.log("Dataset found:", dataset);
         // eslint-disable-next-line
         inputFeatures = transformDatasetToInputFeatures(dataset);
         const transformedFeatures = transformDatasetToInputFeatures(dataset);
-        console.log("Transformed features:", transformedFeatures);
         setFeatures(transformedFeatures);
         //Remove the target variable from the features
         const targetIndex = transformedFeatures.findIndex(feature => feature.name === targetVariable);
@@ -88,10 +81,19 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
     }
   };
 
+  const isChanged = (ogFeature, cfFeature) => {
+    if (String(ogFeature) === "true" && (String(cfFeature) === "true" || String(cfFeature) === "1")) {
+      return false;
+    }
+    if (String(ogFeature) === "false" && (String(cfFeature) === "false" || String(cfFeature) === "0")) {
+      return false;
+    }
+    return String(ogFeature) !== String(cfFeature);;
+  }
+
   const parseCounterfactual = (raw_data) => {
 
-    console.log("Parsing counterfactual...");
-    return raw_data.map(item => {
+    return raw_data.counterfactuals.map(item => {
 
       // Initialize the counterfactual features, changes, and hidden features
       const CounterfactualFeatures = [];
@@ -99,8 +101,13 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
       const hiddenFeatures = [];
 
       // Extract the input and prediction probabilities
-      const inputProbability = (item.original_probability * 100).toFixed(2);
-      const predictionProbability = (item.probability * 100).toFixed(2);
+      const originalData = raw_data.original;
+      const predictionProbability = (item.probability * 100).toFixed(1);
+      const inputProbability = (originalData["probability"] * 100).toFixed(1);
+      const inputClass = originalData[targetVariable];
+      const predictedClass = item[targetVariable];
+
+
 
       // Extract the features
       for (let feature of features) {
@@ -111,19 +118,21 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
         // Create a dictionary for each feature
         const featureDict = {};
 
-        // Extract the feature name, counterfactual, value, and lock status
         featureDict['name'] = feature.name;
-        featureDict['value'] = feature.value;
+
+        // Extract the feature name, counterfactual, value, and lock status
+        // Iterate over the original data 
+        for (const key in originalData) {
+          if (originalData.hasOwnProperty(key)) {
+            const value = originalData[key];
+            if (key === featureDict['name']) {
+              featureDict['value'] = value;
+            }
+          }
+        }
         featureDict['counterfactual'] = item[feature.name];
-        //Change true and false to 1 and 0 in feature value
-        if (featureDict['value'] === true || featureDict['value'] === 'true') {
-          featureDict['value'] = "1";
-        }
-        else if (featureDict['value'] === false || featureDict['value'] === 'false') {
-          featureDict['value'] = "0"
-        }
-        featureDict['locked'] = false;
-        featureDict['changed'] = String(featureDict['value']) !== String(featureDict['counterfactual']);
+        featureDict['locked'] = feature.locked;
+        featureDict['changed'] = isChanged(feature.value, featureDict['counterfactual']);
         if (featureDict['changed']) {
           changes.push(feature.name);
         }
@@ -134,8 +143,6 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
           CounterfactualFeatures.push(featureDict);
         }
 
-        console.log("Feature dict:", featureDict);
-
       }
       // Add to a counterFactual to return
       const counterFactual = {}
@@ -144,13 +151,14 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
       counterFactual['changes'] = changes;
       counterFactual['features'] = CounterfactualFeatures;
       counterFactual['hiddenFeatures'] = hiddenFeatures;
+      counterFactual["inputClass"] = inputClass;
+      counterFactual["predictedClass"] = predictedClass;
       return counterFactual;
     });
 
   }
 
   const generateCounterfactual = async () => {
-    console.log("Generating counterfactual...");
     try {
       const query = features.reduce((acc, feature) => {
 
@@ -189,16 +197,17 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
 
       const data = await response.json();
 
-      console.log("Generated counterfactual:", data);
-
       // Update the state with the received counterfactual
       if (data) {
-        // setSelectedCounterfactual(data);
         const newCounterFactual = parseCounterfactual(data);
-        console.log("New counterfactual:", newCounterFactual);
         const updatedFeatures = newCounterFactual[0].features;
         const updatedHiddenFeatures = newCounterFactual[0].hiddenFeatures;
-        setSelectedCounterfactual({ ...selectedCounterfactual, features: updatedFeatures, hiddenFeatures: updatedHiddenFeatures });
+        const predictionProbability = newCounterFactual[0].predictionProbability;
+        const inputProbability = newCounterFactual[0].inputProbability;
+        const inputClass = newCounterFactual[0].inputClass;
+        const predictedClass = newCounterFactual[0].predictedClass;
+        console.log(data);
+        setSelectedCounterfactual({ predictedClass: predictedClass, inputClass: inputClass, predictionProbability: predictionProbability, inputProbability: inputProbability, features: updatedFeatures, hiddenFeatures: updatedHiddenFeatures });
 
       } else {
         console.error("Counterfactual not generated:", data);
@@ -217,11 +226,11 @@ const Counterfactual = ({ counterfactual, inputFeatures, datasetName, setInputFe
         <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
           <div>
             <Typography variant="body1" style={{ fontFamily: 'Pacifico, cursive' }}>Input</Typography>
-            <Typography variant="h6" style={{ fontFamily: 'Pacifico, cursive' }}>{selectedCounterfactual.inputProbability}% Non-Diabetic</Typography>
+            <Typography variant="h6" style={{ fontFamily: 'Pacifico, cursive' }}>{selectedCounterfactual.inputProbability}% {targetVariable}: {selectedCounterfactual.inputClass}</Typography>
           </div>
           <div>
             <Typography variant="body1" style={{ fontFamily: 'Pacifico, cursive' }}>Counterfactual</Typography>
-            <Typography variant="h6" style={{ color: 'red', fontFamily: 'Pacifico, cursive' }}>{selectedCounterfactual.predictionProbability}% Diabetic</Typography>
+            <Typography variant="h6" style={{ color: 'red', fontFamily: 'Pacifico, cursive' }}>{selectedCounterfactual["predictionProbability"]}% {targetVariable}: {selectedCounterfactual.predictedClass}</Typography>
           </div>
         </div>
         <Divider />
--- counterfactual-ui/src/data/counterfactuals.js
+++ counterfactual-ui/src/data/counterfactuals.js
@@ -2,29 +2,17 @@ const counterfactuals = [
     {
       inputProbability: 63,
       predictionProbability: 79,
+      inputClass:1,
+      predictedClass: 0,
       changes: ['BMI', 'HbA1c level'],
       features: [
-        { name: 'Gender', value: 'Male', counterfactual: 'Male', locked: true, changed: false },
-        { name: 'BMI', value: 'Medium (23.1)', counterfactual: 'Higher (26.3)', locked: true, changed: true },
-        { name: 'HbA1c level', value: 'Low (3.5)', counterfactual: 'Higher (4.6)', locked: true, changed: true },
+        // { name: 'Gender', value: 'Male', counterfactual: 'Male', locked: true, changed: false },
+        // { name: 'BMI', value: 'Medium (23.1)', counterfactual: 'Higher (26.3)', locked: true, changed: true },
+        // { name: 'HbA1c level', value: 'Low (3.5)', counterfactual: 'Higher (4.6)', locked: true, changed: true },
       ],
       hiddenFeatures: [
-        { name: 'Blood Glucose', value: 'Medium (157)', counterfactual: 'Higher (191)', locked: false, changed: true },
-        { name: 'Age', value: 'High (72)', locked: false, changed: false },
-      ],
-    },
-    {
-      inputProbability: 50,
-      predictionProbability: 85,
-      changes: ['Age', 'Blood Glucose'],
-      features: [
-        { name: 'Gender', value: 'Female', counterfactual: 'Female', locked: true, changed: false },
-        { name: 'Age', value: 'Medium (45)', counterfactual: 'Higher (60)', locked: true, changed: true },
-        { name: 'Blood Glucose', value: 'Low (100)', counterfactual: 'Higher (160)', locked: true, changed: true },
-      ],
-      hiddenFeatures: [
-        { name: 'BMI', value: 'Low (18.5)', counterfactual: 'Higher (24)', locked: false, changed: true },
-        { name: 'HbA1c level', value: 'Medium (5.7)', counterfactual: 'Higher (6.2)', locked: false, changed: true },
+        // { name: 'Blood Glucose', value: 'Medium (157)', counterfactual: 'Higher (191)', locked: false, changed: true },
+        // { name: 'Age', value: 'High (72)', locked: false, changed: false },
       ],
     },
   ];
